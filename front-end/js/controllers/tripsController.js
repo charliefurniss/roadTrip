55442612071d3f8a81ac7ee908179007d59b855d
Charlie Furniss <charliefurniss96@gmail.com>
2016-02-20 15:39:00 +0000

    added sidebar and trip form

--- front-end/js/controllers/tripsController.js
+++ front-end/js/controllers/tripsController.js
@@ -4,5 +4,26 @@ angular
 
 TripsController.$inject = ['TripFactory', 'UserFactory', '$state', 'CurrentUser'];
 function TripsController(Trip, User, $state, CurrentUser){
+
+  self.all           = [];
+  self.trip          = {};
+  self.getTrips      = getTrips;
+  self.createTrip    = createTrip;
+
+  function getTrips() {
+    Trip.query(function(data){
+      self.all = data;
+    });
+  }
+
+  function createTrip(){
+    var trip = { trip: self.trip };
+    console.log(trip);
+    Trip.save(trip, function(data){
+      self.all.push(data);
+      self.trip = {};
+      $state.go('trips');
+    });
+  };
   
 }
