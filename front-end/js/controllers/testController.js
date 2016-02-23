a4b49162176aa616ec9e8c39276d5c0a92683870
Charlie Furniss <charliefurniss96@gmail.com>
2016-02-23 15:11:50 +0000

    commit before adding directins directive

--- front-end/js/controllers/testController.js
+++ front-end/js/controllers/testController.js
@@ -8,104 +8,104 @@ function TestController(Trip, User, $state, CurrentUser){
 
   var self = this;
 
- // function initMap() {
- //    var startpoint_place_id = null;
- //    var endpoint_place_id = null;
- //    var travel_mode = google.maps.TravelMode.DRIVING;
- //    var map = new google.maps.Map(document.getElementById('map'), {
- //      mapTypeControl: false,
- //      center: {lat: -33.8688, lng: 151.2195},
- //      zoom: 13
- //    });
- //    var directionsService = new google.maps.DirectionsService;
- //    var directionsDisplay = new google.maps.DirectionsRenderer;
+ function initMap() {
+    var startpoint_place_id = null;
+    var endpoint_place_id = null;
+    var travel_mode = google.maps.TravelMode.DRIVING;
+    var map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeControl: false,
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13
+    });
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
     
- //    directionsDisplay.setMap(map)
+    directionsDisplay.setMap(map)
 
- //    var startpoint_input = document.getElementById('startpoint-input');
- //    var endpoint_input = document.getElementById('endpoint-input');
- //    var modes = document.getElementById('mode-selector');
+    var startpoint_input = document.getElementById('startpoint-input');
+    var endpoint_input = document.getElementById('endpoint-input');
+    var modes = document.getElementById('mode-selector');
 
- //    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(startpoint_input);
- //    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(endpoint_input);
- //    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(startpoint_input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(endpoint_input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
 
- //    var startpoint_autocomplete = new google.maps.places.Autocomplete(startpoint_input);
- //    startpoint_autocomplete.bindTo('bounds', map);
+    var startpoint_autocomplete = new google.maps.places.Autocomplete(startpoint_input);
+    startpoint_autocomplete.bindTo('bounds', map);
     
- //    var endpoint_autocomplete = new google.maps.places.Autocomplete(endpoint_input);
- //    endpoint_autocomplete.bindTo('bounds', map);
+    var endpoint_autocomplete = new google.maps.places.Autocomplete(endpoint_input);
+    endpoint_autocomplete.bindTo('bounds', map);
 
- //    // Sets up a listener on a radio button to change the filter type on Places Autocomplete.
- //    function setupClickListener(id, mode) {
- //      var radioButton = document.getElementById(id);
- //      radioButton.addEventListener('click', function() {
- //        travel_mode = mode;
- //      });
- //    }
- //    setupClickListener('changemode-walking', google.maps.TravelMode.WALKING);
- //    setupClickListener('changemode-transit', google.maps.TravelMode.TRANSIT);
- //    setupClickListener('changemode-driving', google.maps.TravelMode.DRIVING);
+    // Sets up a listener on a radio button to change the filter type on Places Autocomplete.
+    function setupClickListener(id, mode) {
+      var radioButton = document.getElementById(id);
+      radioButton.addEventListener('click', function() {
+        travel_mode = mode;
+      });
+    }
+    setupClickListener('changemode-walking', google.maps.TravelMode.WALKING);
+    setupClickListener('changemode-transit', google.maps.TravelMode.TRANSIT);
+    setupClickListener('changemode-driving', google.maps.TravelMode.DRIVING);
 
- //    function expandViewportToFitPlace(map, place) {
- //      if (place.geometry.viewport) {
- //        map.fitBounds(place.geometry.viewport);
- //      } else {
- //        map.setCenter(place.geometry.location);
- //        map.setZoom(17);
- //      }
- //    }
+    function expandViewportToFitPlace(map, place) {
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+    }
 
- //    startpoint_autocomplete.addListener('place_changed', function() {
- //      var place = startpoint_autocomplete.getPlace();
- //      if (!place.geometry) {
- //        window.alert("Autocomplete's returned place contains no geometry");
- //        return;
- //      }
- //      expandViewportToFitPlace(map, place);
+    startpoint_autocomplete.addListener('place_changed', function() {
+      var place = startpoint_autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      expandViewportToFitPlace(map, place);
 
- //      // If the place has a geometry, store its place ID and route if we have
- //      // the other place ID
- //      startpoint_place_id = place.place_id;
- //      route(startpoint_place_id, endpoint_place_id, travel_mode, directionsService, directionsDisplay);
- //    });
+      // If the place has a geometry, store its place ID and route if we have
+      // the other place ID
+      startpoint_place_id = place.place_id;
+      route(startpoint_place_id, endpoint_place_id, travel_mode, directionsService, directionsDisplay);
+    });
 
- //    endpoint_autocomplete.addListener('place_changed', function() {
- //      var place = endpoint_autocomplete.getPlace();
- //      if (!place.geometry) {
- //        window.alert("Autocomplete's returned place contains no geometry");
- //        return;
- //      }
- //      expandViewportToFitPlace(map, place);
+    endpoint_autocomplete.addListener('place_changed', function() {
+      var place = endpoint_autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      expandViewportToFitPlace(map, place);
 
- //      // If the place has a geometry, store its place ID and route if we have
- //      // the other place ID
- //      endpoint_place_id = place.place_id;
- //      route(startpoint_place_id, endpoint_place_id, travel_mode, directionsService, directionsDisplay);
- //    });
+      // If the place has a geometry, store its place ID and route if we have
+      // the other place ID
+      endpoint_place_id = place.place_id;
+      route(startpoint_place_id, endpoint_place_id, travel_mode, directionsService, directionsDisplay);
+    });
 
- //    function route(startpoint_place_id, endpoint_place_id, travel_mode, directionsService, directionsDisplay) {
- //      if (!startpoint_place_id || !endpoint_place_id) {
- //        return;
- //      }
- //      console.log("start: " + startpoint_place_id);
- //      console.log("end: " + endpoint_place_id);
- //      directionsService.route({
- //        origin: {'placeId': startpoint_place_id},
- //        destination: {'placeId': endpoint_place_id},
- //        travelMode: travel_mode
- //      }, function(response, status) {
- //        console.log(response);
- //        if (status === google.maps.DirectionsStatus.OK) {
- //          directionsDisplay.setDirections(response);
- //        } else {
- //          window.alert('Directions request failed due to ' + status);
- //        }
- //      });
- //    }
+    function route(startpoint_place_id, endpoint_place_id, travel_mode, directionsService, directionsDisplay) {
+      if (!startpoint_place_id || !endpoint_place_id) {
+        return;
+      }
+      console.log("start: " + startpoint_place_id);
+      console.log("end: " + endpoint_place_id);
+      directionsService.route({
+        origin: {'placeId': startpoint_place_id},
+        destination: {'placeId': endpoint_place_id},
+        travelMode: travel_mode
+      }, function(response, status) {
+        console.log(response);
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
 
- //  }
+  }
 
- //  initMap();
+  initMap();
 
 }
--- front-end/js/controllers/tripsController.js
+++ front-end/js/controllers/tripsController.js
@@ -17,24 +17,46 @@ function TripsController(Trip, User, $state, CurrentUser, uiGmapGoogleMapApi){
   self.deleteTrip         = deleteTrip;
   self.editTrip           = editTrip;
   self.updateTrip         = updateTrip;
+  
+  var startpoint_place_id      = null;
+  var startpoint_coords        = {};
+  var endpoint_place_id        = null;
+  var endpoint_coords          = {};
 
   self.title              = "";
 
+  /////////////////////////**** MAP ****////////////////////////////////////
+
   self.map = { 
-    center: { latitude: 45, longitude: -73 
+    center: { 
+      latitude: 45, longitude: -73 
     }, 
     zoom: 8 
   };
 
-  uiGmapGoogleMapApi.then(function(maps) {
-    console.log("got it");
-  });
-
+  // this is an event listener. it listens for a places_changed event – which comes from the Google API – and runs a function 
   var startpointEvents = {
     places_changed: function (searchBox) {
-      console.log(searchBox)
+      var place = searchBox.getPlaces();
+      startpoint_place_id = place[0].place_id;
+      startpoint_coords = getCoords(place[0].geometry.location);
+      console.log(startpoint_coords);
+
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      route(startpoint_place_id, endpoint_place_id);
     }
   }
+
+  function getCoords(locationObject){
+    var location = {};
+    location.lat = locationObject.lat();
+    location.lng = locationObject.lng();
+    return location;
+  }
+
   self.startpointSearchbox = { 
     template:'js/views/searchboxes/startpointSearchbox.tpl.html', 
     events: startpointEvents,
@@ -43,14 +65,57 @@ function TripsController(Trip, User, $state, CurrentUser, uiGmapGoogleMapApi){
 
   var endpointEvents = {
     places_changed: function (searchBox) {
-      console.log(searchBox)
+      var place = searchBox.getPlaces();
+      endpoint_place_id = place[0].place_id;
+      endpoint_coords = getCoords(place[0].geometry.location);
+      console.log(endpoint_coords);
+
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      route(startpoint_place_id, endpoint_place_id);
     }
   }
+
   self.endpointSearchbox = { 
     template:'js/views/searchboxes/endpointSearchbox.tpl.html', 
     events: endpointEvents,
     parentdiv: "endpoint-input"
+     
   };
+
+  uiGmapGoogleMapApi.then(function(maps) {
+    
+  });
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  
+  directionsDisplay.setMap(map);
+
+  function route(startpoint_place_id, endpoint_place_id) {
+    if (!startpoint_place_id || !endpoint_place_id) {
+      return;
+    }
+    console.log("start: " + startpoint_place_id);
+    console.log("end: " + endpoint_place_id);
+    directionsService.route({
+      origin: {'placeId': startpoint_place_id},
+      destination: {'placeId': endpoint_place_id},
+      travelMode: travel_mode
+    }, function(response, status) {
+      console.log(response);
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+
+  /////////////////////API REQUESTS/////////////////////////////////
 
 
   function getTrips(){
