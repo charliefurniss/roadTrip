d5525c49d2e452f0986450bd04e974ab5877c650
Charlie Furniss <charliefurniss96@gmail.com>
2016-02-25 13:49:57 +0000

    more heroku stuff

--- api/config/config.js
+++ api/config/config.js
@@ -1,5 +1,5 @@
 // configure JSON web token and mongo database
 module.exports = {
   'secret': 'jsonwebtokensaregreat',
-  'database': 'mongodb://localhost:27017/roadtrip'
+  'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/roadtrip'
 };
